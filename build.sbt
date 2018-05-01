ac51cedbc83931ceeeb2151505e79d4fcf681f11
Upadhyay <prayag.upd@gmail.com>
2018-05-01 10:50:55 -0700

    add working featherbed http client example

--- build.sbt
+++ build.sbt
@@ -2,7 +2,13 @@ name := "chatServerClient"
 
 scalaVersion := "2.12.5"
 
-libraryDependencies ++= Seq("com.chat.server" %% "chatserverschema" % "0.1-SNAPSHOT")
+libraryDependencies ++= Seq(
+  "com.chat.server" %% "chatserverschema" % "0.1-SNAPSHOT",
+  "io.github.finagle" %% "featherbed" % "0.3.3",
+  "io.github.finagle" %% "featherbed-circe" % "0.3.3"
+)
 
-val ivyLocal = Resolver.file("local", file(Path.userHome.absolutePath + "/.ivy2/local"))(Resolver.ivyStylePatterns)
-externalResolvers := Seq(ivyLocal)
+resolvers += "mvncentral" at "http://central.maven.org/maven2"
+
+//val ivyLocal = Resolver.file("local", file(Path.userHome.absolutePath + "/.ivy2/local"))(Resolver.ivyStylePatterns)
+//externalResolvers := Seq(ivyLocal)
---
+++ src/main/scala/ChatServerClient.scala
@@ -0,0 +1,47 @@
+object ChatServerClient {
+
+  import featherbed.circe._
+  import io.circe.generic.auto._
+  //import shapeless.Coproduct
+  import java.net.URL
+  import com.twitter.util.Await
+
+  case class Post(userId: Int, id: Int, title: String, body: String)
+
+  case class Comment(postId: Int, id: Int, name: String, email: String, body: String)
+
+  class JSONPlaceholderAPI(baseUrl: URL) {
+
+    private val client = new featherbed.Client(baseUrl)
+
+    object posts {
+
+      private val listRequest = client.get("posts").accept("application/json")
+      private val getRequest = (id: Int) => client.get(s"posts/$id").accept("application/json")
+
+      def list() = listRequest.send[Seq[Post]]()
+
+      def get(id: Int) = getRequest(id).send[Post]()
+
+    }
+
+    object comments {
+      private val listRequest = client.get("comments").accept("application/json")
+      private val getRequest = (id: Int) => client.get(s"comments/$id").accept("application/json")
+
+      def list() = listRequest.send[Seq[Comment]]()
+
+      def get(id: Int) = getRequest(id).send[Comment]()
+    }
+
+  }
+
+  def main(args: Array[String]): Unit = {
+    val apiClient = new JSONPlaceholderAPI(new URL("http://jsonplaceholder.typicode.com/"))
+
+    val result = Await.result(apiClient.posts.list())
+
+    println(result)
+  }
+
+}
